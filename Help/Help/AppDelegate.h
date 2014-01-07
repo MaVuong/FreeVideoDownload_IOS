3043e981d8bcfb546b89f27fc10baaee5cf0e731
MaVuong <dethichthien@me.com>
2014-01-07 23:52:10 +0700

    CommitHelp

---
+++ Help/Help/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Help
+//
+//  Created by Sơn Lê Khắc on 1/7/14.
+//  Copyright (c) 2014 Sơn Lê Khắc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
