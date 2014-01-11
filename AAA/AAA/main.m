91c756ce95d51dd6fab896d40861fe96f2ae5779
MaVuong <dethichthien@me.com>
2014-01-12 00:00:24 +0700

    help

---
+++ AAA/AAA/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AAA
+//
+//  Created by Sơn Lê Khắc on 1/10/14.
+//  Copyright (c) 2014 Sơn Lê Khắc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
